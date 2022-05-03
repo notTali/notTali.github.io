438784319465fe98982c1a65522e3dca29605a56
Talifhani <mtalifhani54@gmail.com>
2022-05-03 20:59:32 +0200

    commint to main branch

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+      selector: ".particles",
+      color: ['#b1dcf5','#f5b1c2'],
+      connectParticles: true,
+      maxParticles: 50,
+      sizeVariations: 30,
+      speed: 0.7,
+    });
+};
+
