d54039133bbf30b054f27658d960e7a27d127cf8
notTali <mtalifhani54@gmail.com>
2022-05-22 23:06:54 +0200

    two particles

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     Particles.init({
       selector: ".particles",
-      color: ['#b1dcf5','#f5b1c2','#f36281'],
+      color: ['#b1dcf5','#f5b1c2'],
       connectParticles: true,
       maxParticles: 50,
       sizeVariations: 30,
